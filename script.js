ccf0faf0c608673cd281f4012d04f5cafb27cc31
Youness <oubelkacemy@gmail.com>
2024-01-05 00:00:09 +0100

    saving and retreiving saved theme

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const html = document.querySelector('html');
 const themeSwitch = document.querySelectorAll('.buttons input')
+const screen = document.querySelector('.screen input')
 
-themeSwitch.forEach((radio, index) => {
+
+const savedTheme = localStorage.getItem('theme');
+const isPreferedThemeDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (savedTheme) {
+    changeTheme(savedTheme)
+} else {
+    if (isPreferedThemeDark) {
+        changeTheme('dark')
+    } else {
+        changeTheme('main')
+    }
+}
+
+themeSwitch.forEach((radio) => {
+    if (html.getAttribute('data-theme') == radio.id) {
+        radio.checked = true
+    }
     radio.addEventListener('change', () => {
         if (radio.checked) {
-            if (index == 0) {
-                html.setAttribute('data-theme', 'main')
-            }
-            else if (index == 1) {
-                html.setAttribute('data-theme', 'light')
-            }
-            else {
-                html.setAttribute('data-theme', 'dark')
-            }
+            changeTheme(radio.id)
         }
-
     })
 })
+
+function changeTheme(theme) {
+    html.setAttribute('data-theme', theme)
+    saveToLocalStorage(theme)
+}
+
+function saveToLocalStorage(theme) {
+    localStorage.setItem('theme', theme)
+}
